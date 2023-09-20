a7234ab23a0b454347cac954d99ce271c0398abd
Paulo Sousa <paulopgss@gmail.com>
2023-09-20 20:34:27 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Paulo Sousa on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
