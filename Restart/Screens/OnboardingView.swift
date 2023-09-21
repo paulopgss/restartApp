6bd0557af6a4dac4540e342dcab1a32e0f202729
Paulo Sousa <paulopgss@gmail.com>
2023-09-20 21:20:36 -0300

    feat: :rocket: set structure

---
+++ Restart/Screens/OnboardingView.swift
@@ -0,0 +1,35 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by Paulo Sousa on 20/09/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTY
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    // MARK: - BODY
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Onboarding")
+                .font(.largeTitle)
+            
+            Button(action: {
+                isOnboardingViewActive = false
+            }) {
+                Text("Start")
+            }
+        } //: VSTACK
+    }
+}
+
+// MARK: - PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
